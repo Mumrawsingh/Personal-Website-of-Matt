5b6cdf5d98c27013634883a6cd4e3d4e2de4ae30
Matthew Umrawsingh <matthew.umrawsingh@gmail.com>
2021-10-25 11:59:03 -0400

    IMG Removal Update

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
 
-    function setNewImage() {
-            document.getElementById("sus").src ="AmongUsShh";
-       } 
-    function setOldImage() {
-        document.getElementById("sus").src ="AmongUsSus";
-       }
